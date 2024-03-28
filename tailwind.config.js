f09d2901f99216336b720e2c409ac7ea3aac6c5c
Aashif Ahamed <aashifahdofficial@gmail.com>
2024-03-28 01:17:21 +0000

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ export default {
     ],
 
     theme: {
+        darkMode: 'class',
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
